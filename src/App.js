dfbaef87a43cf53f1f6c7bfb22ba6f444b7759fb
Your Name <you@example.com>
2022-01-28 15:45:54 +0330

    display: none;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
 import './App.css';
+import rrr from"./rrr.png"
+import BITbarg from"./BITbarg.png"
+import ttt from"./ttt.png"
+import fff from"./fff.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerone">
+      <div className="jnm">
+      <img src={BITbarg} alt="hh" className="cvb"/>
+      </div>
+      <div className="uyt">
+       <img src={rrr} alt="hh" className="hhone"/>
+       <button class="kktwo">ورود </button>
+       </div>
+       <div className="der"><div className="kjh" >درباره ما</div></div>
+       
+
+
+
+       
+       
+       
+<div className="azs">
+  <div className="xjo">
+      <div className="jiu" >بیت برگ (شرکت تجارت الکترونیک فرداد شریف) استارتاپی پیشرو در زمینه ارزهای دیجیتال است که از سال 1397 با هدف فراهم اوردن بستری امن و سرع برای خرید و فروش ارزهای دیجیتال شروع به کار کرده است.</div>
+      <img src={ttt} alt="hh" className="frt"/>
+</div>
+
+
+
+  <div className="qbi">
+
+    <div className="fdrs">
+      <div className="hui">:ساعات پاسخگویی</div>
+      <div className="zxs">7روز هفته ، 24 ساعت شبانه روز پاسخگی شما هستیم</div>
+      <div className="fko">021-91079677</div>
     </div>
+
+
+
+
+    <div className="fdrs">
+      <div className="hui">: زمان خرید و فروش</div>
+      <div className="zxs">7روز هفته ، 24 ساعت شبانه روز پاسخگی شما هستیم</div>
+    </div>
+
+
+
+
+    <div className="fdrs">
+      <div className="hui">زمان احراز هویت: </div>
+      <div className="zxs">همه روزه حتی در ایام تعطیل از ساعت 8  صبح  تا 17 عصر</div>
+    </div>
+
+
+
+
+    <div className="fdrs">
+      <div className="hui">علامت تجاری : </div>
+      <div className="zxs">به مو جب اظهار نامه ثبت علامت  تجاری به شماره 139850140001040783علامت تجاری "بیت برگ"در مرکز مالکیت معنوی ج.ا ثبت گردیده است.</div>
+    </div>
+
+
+    <div className="fdrs">
+     <div className="hui">ادرس :</div>
+     <div className="zxs">تهران، مرکز نواوری کسب و کار دانشگاه تهران واقع در دانشکده مدیریت (مراجعات حضوری  با هماهنگی قبلی)</div>
+     <div className="hvr">بازگشت</div>
+    </div>
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+        <div class="cirrcle">
+          <img src={fff} alt="Snow" class="callcenter"/>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
   );
 }
 
